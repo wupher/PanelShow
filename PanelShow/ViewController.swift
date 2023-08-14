855adb4bbd348ecd54abe55929dabc5232d7ff99
wupher <fanwu1977@gmail.com>
2023-08-14 12:32:04 +0800

    Initial Commit

---
+++ PanelShow/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PanelShow
+//
+//  Created by fanwu on 2023/8/14.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
